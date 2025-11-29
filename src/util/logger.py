5328d2ff7d79730d6a748dc658891f8e4b7c1d9f
mothkuri <narsimharao.mothkuri@gmail.com>
2025-11-29 14:18:57 +0530

    adding required directories, files

---
+++ src/util/logger.py
@@ -0,0 +1,16 @@
+import logging
+import sys
+
+def get_logger(name:str = __name__):
+    logger = logging.getLogger(name)
+    
+    if logger.handlers:
+        return logger
+    
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
